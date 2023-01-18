004955fddf92412009f2a4351c2d64136a1273f2
gazanfarli <qznfrli.2001@gmail.com>
2023-01-18 13:06:46 +0400

    first commit

---
+++ src/scenes/geography/index.jsx
@@ -0,0 +1,20 @@
+import { Box } from '@mui/material';
+import Header from '../../components/Header';
+import GeographyChart from '../../components/GeographyChart';
+import { useTheme } from '@mui/material';
+import { tokens } from '../../theme';
+
+const Geopgraphy = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box m='20px'>
+      <Header title="GEOGRAPHY CHART" subtitle='Geography Chart' />
+      <Box height="75vh" border={`1px solid ${colors.grey[100]}`} borderRadius='4px'>
+        <GeographyChart />
+      </Box>
+    </Box>
+  )
+}
+
+export default Geopgraphy
